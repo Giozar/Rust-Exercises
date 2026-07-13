bf8e5e3dd5f538ceef5fd78ee240726c7ca56614
Giovanni Cortazar <manuelgiozar04@gmail.com>
2026-07-13 05:18:23 -0600

    Add: Implementar función hola_mundo y actualizar módulo de entradas y salidas

--- programacion-estructurada/src/main.rs
+++ programacion-estructurada/src/main.rs
@@ -1,10 +1,12 @@
 #![allow(dead_code)]
 
+mod entradas_y_salidas;
 mod operaciones_basicas;
 
 fn main() {
+    entradas_y_salidas::hola_mundo::hola_mundo();
     // Descomenta el algoritmo que quieras probar:
     // operaciones_basicas::conversiones_temperaturas::conversiones_temperaturas();
     // operaciones_basicas::calculos_mentales::calculos_mentales();
-    operaciones_basicas::programando_formulas_raras::prgramando_formulas_raras();
+    // operaciones_basicas::programando_formulas_raras::prgramando_formulas_raras();
 }
